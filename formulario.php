25f3cc9984b8124e3129db151364aa228a2aced9
Fernando Peña <fer.dogs99@gmail.com>
2022-02-11 20:45:49 -0600

    archivos adicionales

--- formulario.php
+++ formulario.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario basicote</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Bienvenido</h1>
+<h1>Bienvenido</h1>
     <ul>
         <li>
             <a href="index.php">Inicio</a>
@@ -16,7 +16,8 @@
             <a href="formulario.php">Formulario</a>
         </li>
     </ul>
-    
-
+    <form action="store.php">
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
