4e6171240c600c9b7cfe1c290e6edee9220e6c1c
Fernando Peña <fer.dogs99@gmail.com>
2022-02-11 21:59:18 -0600

    implementacion de coneccion y formulario

--- conexion.php
+++ conexion.php
@@ -1,4 +1,14 @@
 <?php
 $server = 'localhost';
 $user = 'root';
-$bdname = ''
+$database = 'registros';
+
+//Crear conexion
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database", $user, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
--- formulario.php
+++ formulario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulario</title>
 </head>
 <body>
 <h1>Bienvenido</h1>
@@ -16,7 +16,11 @@
             <a href="formulario.php">Formulario</a>
         </li>
     </ul>
-    <form action="store.php">
+    <form action="store.php" method="POST">
+    <label for="nombre">Nombre</label>  
+    <input type="text" name="nombre" id="Nombre">
+    <label for="apellido">Apellido</label>  
+    <input type="text" name="apellido" id="apellido">
         <input type="submit" value="Enviar">
     </form>
 </body>
